29c7159a5759d982380ee409f1918b29fc901858
niugrace <grace.niu.01@gmail.com>
2018-07-12 16:28:38 +0000

    Done.

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -16,3 +16,17 @@ def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
 end
+
+
+summer_olympics = {:Sydney => 2000, :Athens=>2004, :Beijing=>2008, :London=>2012}
+
+summer_olympics[:Atlanta=>1996]
+
+summer_olympics.each do |location,year|
+ puts "The #{year} summer olympics took place in #{location}."
+end
+
+
+
+
+  
